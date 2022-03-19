8c8b93fa5beee9159e3faf95a631dfdd5d973d70
harun-calisir <101505538+harun-calisir@users.noreply.github.com>
2022-03-20 00:21:49 +0300

    main files

---
+++ src/script.js
@@ -0,0 +1,35 @@
+const dogImage = document.querySelector(".dogImage")
+const dogSound = document.querySelector(".dogSound")
+dogImage.addEventListener("click", () => {
+    dogSound.play();
+})
+
+const lionImage = document.querySelector(".lionImage")
+const lionSound = document.querySelector(".lionSound")
+lionImage.addEventListener("click", () => {
+    lionSound.play();
+})
+
+const parrotImage = document.querySelector(".parrotImage")
+const parrotSound = document.querySelector(".parrotSound")
+parrotImage.addEventListener("click", () => {
+    parrotSound.play();
+})
+
+const ravenImage = document.querySelector(".ravenImage")
+const ravenSound = document.querySelector(".ravenSound")
+ravenImage.addEventListener("click", () => {
+    ravenSound.play();
+})
+
+const catImage = document.querySelector(".catImage")
+const catSound = document.querySelector(".catSound")
+catImage.addEventListener("click", () => {
+    catSound.play();
+})
+
+const monkeyImage = document.querySelector(".monkeyImage")
+const monkeySound = document.querySelector(".monkeySound")
+monkeyImage.addEventListener("click", () => {
+    monkeySound.play();
+})
